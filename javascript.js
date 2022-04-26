adcfc2ee5c86ee254ee8cae07a44ff495779bce9
Lukas <Lukas.eriksson@skola.taby.se>
2022-04-26 22:26:55 +0200

    crash detation

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ const gravity = 0.2;
 class Player {
   constructor() {
     this.height = 50;
-    this.with = 50;
+    this.width = 50;
 
     this.position = {
       x: 30,
@@ -22,7 +22,7 @@ class Player {
   }
   playerDraw() {
     context.fillStyle = "green";
-    context.fillRect(this.position.x, this.position.y, this.with, this.height);
+    context.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   updatePos() {
@@ -36,30 +36,62 @@ class Player {
   }
 }
 
-class Opsticals {
+class Obsticals {
   constructor() {
-    this.width = 40;
+    this.width = 600;
     this.height = 80;
 
     this.position = {
-      x: 200,
+      x: 400,
       y: canvas.height - this.height,
     };
+    this.acceleration = {
+      x: 0.5,
+      y: 0,
+    };
+    this.speed = {
+      x: 0.05,
+      y: 0,
+    };
   }
-  opsticalDraw() {
+
+  obsticalDraw() {
     context.fillStyle = "black";
     context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  updateObstical() {
+    this.obsticalDraw();
+    this.position.x -= this.acceleration.x;
+    this.acceleration.x += this.speed.x;
   }
 }
 
 const player = new Player();
-const opsticals = new Opsticals();
+const obstical = new Obsticals();
 
 function animation() {
   requestAnimationFrame(animation);
   context.clearRect(0, 0, canvas.width, canvas.height);
   player.updatePos();
-  opsticals.opsticalDraw();
+  obstical.updateObstical(); // ändra sen till updateObstical
+
+  //crash detection
+  if (
+    player.position.y + player.height <= obstical.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      obstical.position.y &&
+    player.position.x + player.width >= obstical.position.x &&
+    player.position.x <= obstical.position.x + obstical.width
+  ) {
+    player.velocity.y = 0; // ändra till att man dör vid nud istället för att åka på objekt
+    console.log("Nuddade topen av blocket");
+  } else if (
+    player.position.x + player.width >= obstical.position.x &&
+    player.position.y + player.velocity.y >= obstical.position.y
+  ) {
+    console.log("nudd frånt");
+    obstical.acceleration.x = 0; //ändra till att man dör vid nud istället för att åka på objekt
+  }
 }
 
 animation();
